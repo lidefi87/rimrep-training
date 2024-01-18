771d5e1aa9e6a69ff36b5a5d90b5552be336cb86
lidefi87 <lidefi87@hotmail.com>
2024-01-18 22:00:40 +1100

    Adding new libraries used in new scripts

--- Installing_R_libraries.R
+++ Installing_R_libraries.R
@@ -9,7 +9,8 @@ checking_libraries <- function(){
   #List of packages needed to run all notebooks in repository
   packages_required <- c("arrow", "tidyverse", "wkb", "sf", "rnaturalearth", 
                          "leaflet", "tictoc", "DT", "wordcloud2", "janitor",
-                         "magrittr", "tm", "httr", "httr2", "terra", "tcltk")
+                         "magrittr", "tm", "httr", "httr2", "terra", "tcltk",
+                         "usethis", "tidyterra")
   
   #Checking packages installed in local machine
   packages_local <- installed.packages()
